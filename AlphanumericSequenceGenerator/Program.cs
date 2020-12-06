94612e14a9edf58eb1313a808b0305d93dddf9f2
Philippe Mathieu <p.mathieu@onepointLU.onmicrosoft.com>
2020-12-06 09:14:00 +0100

    Review outputs in program

--- AlphanumericSequenceGenerator/Program.cs
+++ AlphanumericSequenceGenerator/Program.cs
@@ -10,24 +10,19 @@ namespace AlphanumericSequenceGenerator
             if (args.Length != 2)
             {
                 Console.WriteLine("Wrong number of arguments.");
-                Console.WriteLine("Syntax: AlphanumericEnumerationGenerator lowerbound upperbound");
-                Console.WriteLine("        Both lowerbound and upperbound must contains only letters (case sensitive) and digits.");
+                Console.WriteLine("Syntax: AlphanumericSequenceGenerator lower_bound upper_bound");
                 return;
             }
 
-            Console.WriteLine("Alphanumeric Enumeration Generator is starting...");
-
             var sequencer = new AlphanumericSequencer(args[0], args[1]);
 
-            // Generate the enumeration
+            // Generate the sequence
             var sequence = sequencer.GetSequence();
 
             foreach (var item in sequence)
             {
                 Console.WriteLine(item);
             }
-
-            Console.WriteLine("Ended alphanumeric enumeration generation.");
         }
     }
 }
